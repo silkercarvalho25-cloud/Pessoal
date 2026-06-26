d33e79a0daee7db2d1e19dcb6cdb925933de8ff0
silker_carvalho <silkercarvalho25@gmail.com>
2026-06-25 21:32:18 -0300

    docs: add  simple Class code examples

---
+++ python/desafios/exercicio_class.py
@@ -0,0 +1,17 @@
+class Cachorro:
+    def __init__(self, nome, raca):
+        self.nome = nome
+        self.raca = raca
+
+    def latir(self):
+        print(f"{self.nome} está latindo: Au au!")
+
+
+dog_1 = Cachorro("Rex", "Shitsu")
+
+dog_2 = Cachorro("Dexter", "Shitsu")
+
+dog_1.latir()
+
+dog_2.latir()
+    
